41e95e6f6a28bc9ad2a161f0d3c5beac41941c4b
hamidhosen42 <mdhamidhosen8444@gmail.com>
2022-03-26 20:07:45 +0600

    final code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import BookShop from './Component/BookShop/BookShop';
 import Header from './Component/Header/Header';
+import Questions from './Component/Questions/Questions';
 
 function App() {
   return (
     <div>
       <Header></Header>
       <BookShop></BookShop>
+      <Questions></Questions>
     </div>
   );
 }
